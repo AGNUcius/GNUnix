dd0d1f5c19ce18fb64b7653f463b81c4441155f6
Patrick Anderson <agnucius@gmail.com>
2012-06-15 12:30:56 -0600

    cleanup

--- NoteMacs/NoteMacs.el
+++ NoteMacs/NoteMacs.el
@@ -814,10 +814,10 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "#555" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight bold :height 120 :width normal :foundry "unknown" :family "FreeMono"))))
+ '(default ((t (:stipple nil :background "#555" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 142 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
  '(cursor ((t (:background "yellow"))))
  '(font-lock-comment-face ((t (:foreground "#333"))))
- '(region (( t (:inverse-video t))))
+ '(region ((t (:inverse-video t))))
  '(trailing-whitespace ((t (:background "#666")))))
 
 
