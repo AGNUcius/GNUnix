bacfbdd5da32a3634311a724ae0fe5604e72af38
patrick.anderson <patrick.anderson@rackwareinc.com>
2014-09-10 16:07:59 -0600

    fix mute button

--- bin/wim.list.cmd
+++ bin/wim.list.cmd
@@ -1,1 +1,2 @@
 dism /Get-MountedWimInfo
+
--- bin/wim.mount.cmd
+++ bin/wim.mount.cmd
@@ -1,2 +1,6 @@
-if not exist "%~pn1" MD "%~pn1"
-dism /Mount-Wim /WimFile:%~f1 /index:1 /MountDir:%~pn1  /ReadOnly
+set MountDir=%~pn1
+
+if not '%2'=='' set MountDir=%2
+if not exist "%MountDir%" MD "%MountDir%"
+dism /Mount-Wim /WimFile:%~f1 /index:1 "/MountDir:%MountDir%"
+
